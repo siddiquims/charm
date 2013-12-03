ab7c15b791f42f90fa41dfd0557172ce520933f8
Athemis <alexander.minges@gmail.com>
2013-12-03 10:48:47 +0100

    Prepare for tagging release 0.1: Add comments to LibCharm.IO

--- LibCharm/IO/__init__.py
+++ LibCharm/IO/__init__.py
@@ -7,22 +7,28 @@ except ImportError as e:
 
 def load_file(filename, file_format="fasta"):
     """
-    Load sequence from file in FASTA file_format
-    filename - Path and filename of input sequence file
+    Load sequence from file and returns sequence as Bio.Seq object
+    filename    - String; Path and filename of input sequence file
+    file_format - String; Format to be used. Refer to Biopython docs for available formats. Defaults to 'fasta'
     """
     content = None
     try:
+        # assume sequence is DNA
         content = SeqIO.read(filename, file_format, IUPAC.unambiguous_dna)
     except ValueError as e:
+        # if this fails, try RNA instead
         print('ERROR: {}'.format(e))
         try:
             content = SeqIO.read(filename, file_format, IUPAC.unambiguous_rna)
         except ValueError as e:
+            # if this fails, too, raise exception and exit with error code 1
             print('ERROR: {}'.format(e))
             exit(1)
 
+    # if some kind of data could be read, return the sequence object
     if content:
         seq = content.seq
         return seq
+    # else return None
     else:
         return None
